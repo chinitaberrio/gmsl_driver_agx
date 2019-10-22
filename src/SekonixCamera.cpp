9bfcd0b49710b1f55f5dcb7ac0d4bfde020ab227
Mert Çolak <mert.colak@leomuhendislik.com>
2019-10-22 12:10:39 +0300

    node name updated

--- src/SekonixCamera.cpp
+++ src/SekonixCamera.cpp
@@ -2,11 +2,11 @@
 
 SekonixCamera::SekonixCamera(ros::NodeHandle &nh_in, DeviceArguments CameraArguments) : nh_(nh_in) {
 
-  nh_in.param("/sekonix_camera/image_width", image_width_, int(640));
-  nh_in.param("/sekonix_camera/image_height", image_height_, int(480));
-  nh_in.param("/sekonix_camera/image_buffer", pub_buffer_, int(5));
-  nh_in.param("/sekonix_camera/image_compressed", img_compressed_, true);
-  nh_in.param("/sekonix_camera/image_compressed_quality", jpeg_quality_, int(70));
+  nh_in.param("/sekonix_camera_node/image_width", image_width_, int(640));
+  nh_in.param("/sekonix_camera_node/image_height", image_height_, int(480));
+  nh_in.param("/sekonix_camera_node/image_buffer", pub_buffer_, int(5));
+  nh_in.param("/sekonix_camera_node/image_compressed", img_compressed_, true);
+  nh_in.param("/sekonix_camera_node/image_compressed_quality", jpeg_quality_, int(70));
 
 
   // read ros param for camera info publish
@@ -33,14 +33,14 @@ SekonixCamera::SekonixCamera(ros::NodeHandle &nh_in, DeviceArguments CameraArgum
   std::string fifo_size_value = "";
   std::string slave_value = "";
   // reading new configuration
-  nh_in.param<std::string>("/sekonix_camera/type_a", type_a_value,"ar0231-rccb-bae-sf3324");
-  nh_in.param<std::string>("/sekonix_camera/type_b", type_b_value,"ar0231-rccb-bae-sf3324");
-  nh_in.param<std::string>("/sekonix_camera/type_c", type_c_value,"ar0231-rccb-bae-sf3324");
-  nh_in.param<std::string>("/sekonix_camera/type_d", type_d_value,"ar0231-rccb-bae-sf3324");
-  nh_in.param<std::string>("/sekonix_camera/selector_mask", selector_mask_value,"0001");
-  nh_in.param<std::string>("/sekonix_camera/cross_csi_sync", cross_csi_sync_value,"0");
-  nh_in.param<std::string>("/sekonix_camera/fifo_size", fifo_size_value,"3");
-  nh_in.param<std::string>("/sekonix_camera/slave", slave_value,"0");
+  nh_in.param<std::string>("/sekonix_camera_node/type_a", type_a_value,"ar0231-rccb-bae-sf3324");
+  nh_in.param<std::string>("/sekonix_camera_node/type_b", type_b_value,"ar0231-rccb-bae-sf3324");
+  nh_in.param<std::string>("/sekonix_camera_node/type_c", type_c_value,"ar0231-rccb-bae-sf3324");
+  nh_in.param<std::string>("/sekonix_camera_node/type_d", type_d_value,"ar0231-rccb-bae-sf3324");
+  nh_in.param<std::string>("/sekonix_camera_node/selector_mask", selector_mask_value,"0001");
+  nh_in.param<std::string>("/sekonix_camera_node/cross_csi_sync", cross_csi_sync_value,"0");
+  nh_in.param<std::string>("/sekonix_camera_node/fifo_size", fifo_size_value,"3");
+  nh_in.param<std::string>("/sekonix_camera_node/slave", slave_value,"0");
 
 
   // setting new configurations
