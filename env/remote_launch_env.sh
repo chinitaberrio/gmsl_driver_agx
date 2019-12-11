83f656977a811d804fffc2566ac87c14b4f039a4
Mert Çolak <colakmert01995@gmail.com>
2019-12-11 15:35:41 +0300

    fix hostname

--- env/remote_launch_env.sh
+++ env/remote_launch_env.sh
@@ -4,6 +4,6 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib:/usr/lib/aarch64-linux-gnu
 source /opt/ros/kinetic/setup.bash
 source /home/nvidia/projects/gmsl_ws/devel/setup.bash
 
-export ROS_HOSTNAME="xA"
+export ROS_HOSTNAME="xA.local"
 
 exec "$@"
