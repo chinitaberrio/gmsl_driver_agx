4c355ca900a5abfcadc5791493d861a92fb3215e
Mert Çolak <colakmert01995@gmail.com>
2019-12-11 15:33:12 +0300

    fix setup.bash location

--- env/remote_launch_env.sh
+++ env/remote_launch_env.sh
@@ -2,7 +2,7 @@
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib:/usr/lib/aarch64-linux-gnu
 source /opt/ros/kinetic/setup.bash
-source /home/projects/gmsl_ws/devel/setup.bash
+source /home/nvidia/projects/gmsl_ws/devel/setup.bash
 
 export ROS_HOSTNAME="xA"
 
