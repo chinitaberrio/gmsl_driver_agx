9831113b500e489cebdd4247ae2c221278b82624
Mert Çolak <colakmert01995@gmail.com>
2019-12-11 15:40:01 +0300

    add rosmaster address

--- env/remote_launch_env.sh
+++ env/remote_launch_env.sh
@@ -4,6 +4,7 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib:/usr/lib/aarch64-linux-gnu
 source /opt/ros/kinetic/setup.bash
 source /home/nvidia/projects/gmsl_ws/devel/setup.bash
 
-export ROS_HOSTNAME="xA.local"
+export ROS_HOSTNAME="xA"
+export ROS_MASTER_URI=http://10.42.0.29:11311 
 
 exec "$@"
