72eb4d1d4c3d2899a627656b0f7f8166618953e0
Mert Çolak <colakmert01995@gmail.com>
2019-12-11 15:22:18 +0300

    source ros kinetic

--- env/remote_launch_env.sh
+++ env/remote_launch_env.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib:/usr/lib/aarch64-linux-gnu
+source /opt/ros/kinetic/setup.bash
 source /home/projects/gmsl_ws/devel/setup.bash
 
 export ROS_HOSTNAME="xA"
